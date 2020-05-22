3694e91d6e7bf6d230abbf69fbca75eb1c8fa763
Lovish Hamal <lovishhamal123@gmail.com>
2020-05-22 20:36:58 +0545

    updated routes

---
+++ routes/routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const QuizDb = require('../dbModel');
+
+router.get('/getquestions', async (req, res) => {
+	try {
+		const find = await QuizDb.find();
+
+		return res.json(find);
+	} catch (error) {}
+});
+
+router.post('/add', async (req, res) => {
+	try {
+		console.log('done ,');
+
+		const { question } = req.body;
+		const { options } = req.body;
+		const quiz = new QuizDb({
+			questions: question,
+			values: options
+		});
+		await quiz.save();
+		return res.status(200).json('success');
+	} catch (error) {}
+});
+
+router.get('/addoption/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const find = await QuizDb.findOne({ _id: id });
+		return res.json(find);
+	} catch (error) {}
+});
+
+router.put('/update/', async (req, res) => {
+	try {
+		const { checkOptions, Qid } = req.body;
+
+		for (let i in checkOptions) {
+			const update = await QuizDb.findOneAndUpdate(
+				{
+					_id: Qid
+				},
+				{ $push: { values: { item: checkOptions[i], value: false } } }
+			);
+		}
+		res.status(200).json('updated');
+	} catch (error) {}
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const port = 5000;
 const cors = require('cors');
 
 const mongoose = require('mongoose');
-const QuizDb = require('./dbModel');
 
 app.use(cors());
 app.use(express.json());
@@ -22,49 +21,7 @@ mongoose
 		console.log('Mongo not connected');
 	});
 
-app.post('/add', async (req, res) => {
-	try {
-		const { question, options } = req.body;
-
-		const quiz = new QuizDb({
-			questions: question,
-			values: options
-		});
-		await quiz.save();
-		return res.status(200).json('success');
-	} catch (error) {}
-});
-
-app.get('/getquestions', async (req, res) => {
-	try {
-		const find = await QuizDb.find();
-		return res.json(find);
-	} catch (error) {}
-});
-
-app.get('/addoption/:id', async (req, res) => {
-	try {
-		const { id } = req.params;
-		const find = await QuizDb.findOne({ _id: id });
-		return res.json(find);
-	} catch (error) {}
-});
-
-app.put('/update/', async (req, res) => {
-	try {
-		const { checkOptions, Qid } = req.body;
-
-		for (let i in checkOptions) {
-			const update = await QuizDb.findOneAndUpdate(
-				{
-					_id: Qid
-				},
-				{ $push: { values: { item: checkOptions[i], value: false } } }
-			);
-		}
-		res.status(200).json('updated');
-	} catch (error) {}
-});
+app.use('/', require('./routes/routes'));
 
 app.listen(port, () => {
 	console.log(`Server listening at port ${5000}`);
